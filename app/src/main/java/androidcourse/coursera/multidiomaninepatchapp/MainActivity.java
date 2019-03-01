310a14a3778423fa6295a6b223f917c7f3eb30fc
Elianna <eliannabp@gmail.com>
2019-03-01 20:53:01 +0100

    Texto para probar commit en branch

--- app/src/main/java/androidcourse/coursera/multidiomaninepatchapp/MainActivity.java
+++ app/src/main/java/androidcourse/coursera/multidiomaninepatchapp/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Test a pull request into branch
         setContentView(R.layout.activity_main);
     }
 }
