e621e98a16fbc170363c25faa1c63b5e9579af30
Elianna <eliannabp@gmail.com>
2019-03-01 20:32:32 +0100

    Commit inicial

--- app/src/main/java/androidcourse/coursera/multidiomaninepatchapp/MainActivity.java
+++ app/src/main/java/androidcourse/coursera/multidiomaninepatchapp/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Test a pull to new branch
         setContentView(R.layout.activity_main);
     }
 }
